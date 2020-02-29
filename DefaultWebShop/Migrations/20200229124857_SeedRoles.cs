452659eac9afea8147a7c307b041695bfefcb75b
Pavel <pavel.ivanko@hotmail.com>
2020-02-29 13:51:26 +0100

    Seed roles (user and admin)

---
+++ DefaultWebShop/Migrations/20200229124857_SeedRoles.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DefaultWebShop.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO AspNetRoles (Id, Name) VALUES (1, 'Admin')");
+            migrationBuilder.Sql("INSERT INTO AspNetRoles (Id, Name) VALUES (2, 'User')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM AspNetRoles");
+        }
+    }
+}

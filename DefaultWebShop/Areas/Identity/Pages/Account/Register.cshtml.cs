ab68aacdf977002c1b8ba13ee5edcd3574e54c4e
Pavel <pavel.ivanko@hotmail.com>
2020-03-09 04:17:51 +0100

    Create product as admin

--- DefaultWebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DefaultWebShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -55,6 +55,9 @@ namespace DefaultWebShop.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [DataType(DataType.Date)]
+            public DateTime Birthdate { get; set; }
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -78,7 +81,7 @@ namespace DefaultWebShop.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Birthdate = Input.Birthdate };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
--- DefaultWebShop/Controllers/AdminController.cs
+++ DefaultWebShop/Controllers/AdminController.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DefaultWebShop.Services;
+using DefaultWebShop.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DefaultWebShop.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IGenreService _genreService;
+        private readonly IProductService _productService;
+        public AdminController(IGenreService genreService, IProductService productService)
+        {
+            _genreService = genreService;
+            _productService = productService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> CreateProduct()
+        {
+            await LoadGenres();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductViewModel model, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                await LoadGenres();
+                return View(model);
+            }
+            await _productService.CreateProduct(model, image);
+            return View(nameof(Index));
+        }
+        private async Task LoadGenres()
+        {
+            var genres = await _genreService.GetGenres();
+            ViewBag.Genres = new SelectList(genres, "GenreID", "Name");
         }
     }
 }
